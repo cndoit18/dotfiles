96bf060465c2c1589449990e11b001c710bc26f5
cndoit18 <cndoit18@outlook.com>
2025-03-19 18:10:11 +0800

    feat: support linux

--- home/.install-prerequisites.sh
+++ home/.install-prerequisites.sh
@@ -16,6 +16,17 @@ Darwin)
 		read -p "Loggin to 1Password and enable 1Password-CLI integration. Enter to continue..." -n 1 -r
 	fi
 	;;
+Linux)
+	if [ ! "$(command -v op)" ]; then
+		curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+		echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+		sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+		curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+		sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+		curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+		sudo apt update && sudo apt install -y 1password-cli
+	fi
+	;;
 *)
 	echo "unsupported OS"
 	exit 1
