e63f0bbf3b64063b0b355d461c501ed9c768dc22
cndoit18 <cndoit18@outlook.com>
2025-03-20 18:21:29 +0800

    refactor: refactor 1Password install logic to check 'op' command existence

--- home/.install-prerequisites.sh
+++ home/.install-prerequisites.sh
@@ -8,12 +8,13 @@ Darwin)
 		eval "$(env brew shellenv)"
 	fi
 
-	brew list 1password &>/dev/null || brew install --cask 1password
-	brew list 1password-cli &>/dev/null || brew install 1password-cli
-
-	if [[ $(op account list | wc -l) -lt 1 ]]; then
-		open -a '1Password'
-		read -p "Loggin to 1Password and enable 1Password-CLI integration. Enter to continue..." -n 1 -r
+	if [ ! "$(command -v op)" ]; then
+		brew install --cask 1password
+		brew install 1password-cli
+		if [[ $(op account list | wc -l) -lt 1 ]]; then
+			open -a '1Password'
+			read -p "Loggin to 1Password and enable 1Password-CLI integration. Enter to continue..." -n 1 -r
+		fi
 	fi
 	;;
 Linux)
