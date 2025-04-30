9a0bccb0a6abbdf44fad5b2d773ddb66190504e2
cndoit18 <cndoit18@outlook.com>
2025-04-30 18:11:26 +0800

    feat: add notebook-navigator configuration and JupyterCreate command with custom setup options

--- home/private_dot_config/exact_nvim/exact_lua/exact_plugins/notebook_navigator.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_plugins/notebook_navigator.lua
@@ -42,7 +42,26 @@ return {
 	event = "VeryLazy",
 	config = function()
 		local nn = require("notebook-navigator")
-		nn.setup({})
+		nn.setup({
+			repl_provider = "iron",
+			syntax_highlight = true,
+			cell_highlight_group = "Folded",
+		})
+		vim.api.nvim_create_user_command("JupyterCreate", function(opts)
+			-- 获取用户传入的参数
+			local full_path = opts.args
+			-- 提取文件夹路径和文件名
+			local dir_path = full_path:match("(.*/)") or ""
+			local file_name = full_path:match("[^/]+$")
+			-- 构建要执行的命令
+			local command = string.format(
+				'!echo \'{"cells":[],"metadata":{"kernelspec":{"display_name":"Python 3 (ipykernel)","language":"python","name":"python3"},"language_info":{"codemirror_mode":{"name":"ipython","version":3},"file_extension":".py","mimetype":"text/x-python","name":"python","nbconvert_exporter":"python","pygments_lexer":"ipython3"}},"nbformat":4,"nbformat_minor":5}\' | jupyter nbconvert --to notebook --execute --stdin --output-dir=%s --output=%s',
+				dir_path,
+				file_name
+			)
+			-- 执行命令
+			vim.cmd(command)
+		end, { nargs = 1, complete = "file", desc = "Create a Jupyter notebook with the given path and filename" })
 
 		vim.api.nvim_create_user_command("RunCell", function()
 			nn.run_cell()
