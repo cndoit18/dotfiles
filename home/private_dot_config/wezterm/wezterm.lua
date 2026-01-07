4330c2aef6ea149dbb73eaf1167143a8a9a46812
cndoit18 <cndoit18@outlook.com>
2026-01-07 20:23:24 +0800

    chore(config): improve terminal configuration
    - Disable terminal beep in zsh (unsetopt beep)
    - Update WezTerm config to use new config_builder() API

--- home/private_dot_config/wezterm/wezterm.lua
+++ home/private_dot_config/wezterm/wezterm.lua
@@ -1,24 +1,22 @@
 local wezterm = require("wezterm")
-local config = {}
+local config = wezterm.config_builder()
 
-config = {
-	-- color scheme can be found here: https://github.com/mbadolato/iTerm2-Color-Schemes/tree/master/wezterm
-	color_scheme = "Solarized Dark (Gogh)",
-	font = wezterm.font("DroidSansM Nerd Font"),
-	enable_tab_bar = true,
-	use_fancy_tab_bar = true,
-	hide_tab_bar_if_only_one_tab = true,
-	tab_bar_at_bottom = false,
-	use_ime = true,
-	native_macos_fullscreen_mode = true,
-	window_padding = {
-		left = 0,
-		right = 0,
-		top = 0,
-		bottom = 0,
-	},
-	leader = { key = "b", mods = "CTRL" },
+-- color scheme can be found here: https://github.com/mbadolato/iTerm2-Color-Schemes/tree/master/wezterm
+config.color_scheme = "Solarized Dark (Gogh)"
+config.font = wezterm.font("DroidSansM Nerd Font")
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = false
+config.use_ime = true
+config.native_macos_fullscreen_mode = true
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
+config.leader = { key = "b", mods = "CTRL" }
 
 require("wez-tmux.plugin").apply_to_config(config, {})
 return config
