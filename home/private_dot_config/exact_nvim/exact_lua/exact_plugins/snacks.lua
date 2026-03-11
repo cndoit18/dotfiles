a284a5ad2aecf9895ca0cb5112cdf7ebf7a36621
cndoit18 <cndoit18@outlook.com>
2026-03-11 13:52:26 +0800

    chore(nvim): add bigfile configuration for snacks plugin

--- home/private_dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
+++ home/private_dot_config/exact_nvim/exact_lua/exact_plugins/snacks.lua
@@ -4,6 +4,24 @@ return {
 	lazy = false,
 	---@type snacks.Config
 	opts = {
+		bigfile = {
+			notify = true,
+			size = 2 * 1024 * 1024,
+			line_length = 1500,
+			setup = function()
+				vim.cmd("LspStop")
+				vim.treesitter.stop()
+				vim.opt.swapfile = false
+				vim.opt.writebackup = false
+				vim.opt.spell = false
+				vim.opt.laststatus = 1
+				vim.opt.foldmethod = "manual"
+				local ok, cmp = pcall(require, "cmp")
+				if ok then
+					cmp.setup.buffer({ enabled = false })
+				end
+			end,
+		},
 		scroll = {},
 		lazygit = {},
 		gitbrowse = {
